5fec890843f99e786186bbc2008ab23ef751bd86
hazelanncreator <solanohazel123@gmail.com>
2025-09-15 16:02:04 +0800

    adding comments

--- app.py
+++ app.py
@@ -6,21 +6,20 @@ num2 = int(input("Enter the Second Number for addition :"))
 sum_result = num1 + num2
 print(f"Addition Results of {num1} + {num2} =  {sum_result}")
 
-
-
-
-
 '''
 
+
+#exponents
 '''
 num1 = int(input("Enter a number:")) 
 num12 = int(input("Enter a number: "))
 
 result = num1 ** num12
 print(f"Results of {num1} ** {num12} = {result}   ") 
-
-
 '''
+
+
+#Division
 '''
 num3 = int(input("Enter the first number for division : "))
 num4 = int(input("Enter the second number for division :"))
