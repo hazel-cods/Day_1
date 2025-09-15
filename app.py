7697d40714a883bcd42d7f5840fcde03959c38c1
hazelanncreator <solanohazel123@gmail.com>
2025-09-15 15:38:29 +0800

    celsius to fahrenheit

--- app.py
+++ app.py
@@ -71,3 +71,12 @@ conv = 0.621371
 miles = kilometers * conv
 print(f" {kilometers} Kilometers is = {miles} Miles")
 '''
+
+
+# conversion formula : fahrenheit = (celsius * 9/5) + 32 
+'''
+celsius = float(input("Enter temperature in Celsius :"))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f" {celsius} degrees celsius is {fahrenheit} Fahrenheit")
+'''
