a9ee157db55bbc705f3f9f8b0f044542764e9347
hazelanncreator <solanohazel123@gmail.com>
2025-09-15 14:49:23 +0800

    area of the triangle

--- app.py
+++ app.py
@@ -35,4 +35,11 @@ else:
 
 
 
+'''
+base = int(input("Enter the base of the triangle: "))
+height = int(input("Enter the height of the triangle: "))
 
+area = 0.5 * base * height
+print(f"The area of the triangle is: {area}")
+
+'''
