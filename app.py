c4376a6c8c1f87b68a28e678f263a26fc9262e70
hazelanncreator <solanohazel123@gmail.com>
2025-09-15 14:23:28 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,7 @@
+ #add numbers
+
+num1 = int(input("Enter the First number for addition :"))
+num2 = int(input("Enter the Second Number for addition :"))
+
+sum_result = num1 + num2
+print(f"Addition Results of {num1} + {num2} =  {sum_result}")
