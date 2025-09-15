f8edd427bc431aa00d41a269c87a66de5c1971b8
hazelanncreator <solanohazel123@gmail.com>
2025-09-15 14:33:31 +0800

    exponent

--- app.py
+++ app.py
@@ -11,8 +11,15 @@ print(f"Addition Results of {num1} + {num2} =  {sum_result}")
 
 
 '''
+
+'''
 num1 = int(input("Enter a number:")) 
 num12 = int(input("Enter a number: "))
 
 result = num1 ** num12
 print(f"Results of {num1} ** {num12} = {result}   ") 
+
+
+'''
+
+
